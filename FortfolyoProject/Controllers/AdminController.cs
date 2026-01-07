d3c6decd3aa1db6642f6d5dea283b1cdf46d667f
YİĞİT ÖRÜCÜ <orucuyigit@gmail.com>
2026-01-07 14:50:04 +0300

    Added a new Admin Thema

---
+++ FortfolyoProject/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FortfolyoProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult IndexLayout()
+        {
+            return View();
+        }
+
+
+        public IActionResult Experience()
+        {
+            return View();
+        }
+    }
+}
---
+++ FortfolyoProject/ViewComponents/AdminComponents/AdminHeadViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FortfolyoProject.ViewComponents.AdminComponents
+{
+    public class AdminHeadViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Views/Shared/Components/AdminComponents/AdminHead.cshtml");
+        }
+    }
+}
